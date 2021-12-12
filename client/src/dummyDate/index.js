302fe32c05d9ad08e50fc01db6090b28af7976d8
bejeju <bejejupark@gmail.com>
2021-12-12 21:48:07 +0900

    21.12.12

--- client/src/dummyDate/index.js
+++ client/src/dummyDate/index.js
@@ -8,6 +8,14 @@ export default {
         {id: 6, title: 'title', comments: '', src: 'https://images.pexels.com/photos/4725133/pexels-photo-4725133.jpeg'},
         {id: 7, title: 'title', comments: '', src: 'https://dummyimage.com/600x400/000/000fe3'},
         {id: 8, title: 'title', comments: '', src: 'https://dummyimage.com/600x400/000/ed0c0c'},
+        {id: 9, title: 'title', comments: '', src: 'https://images.pexels.com/photos/3348748/pexels-photo-3348748.jpeg'},
+        {id: 10, title: 'title', comments: '', src: 'https://images.pexels.com/photos/3812944/pexels-photo-3812944.jpeg'},
+        {id: 11, title: 'title', comments: '', src: 'https://images.pexels.com/photos/2100063/pexels-photo-2100063.jpeg'},
+        {id: 12, title: 'title', comments: '', src: 'https://images.pexels.com/photos/3228213/pexels-photo-3228213.jpeg'},
+        {id: 13, title: 'title', comments: '', src: 'https://images.pexels.com/photos/1385472/pexels-photo-1385472.jpeg'},
+        {id: 14, title: 'title', comments: '', src: 'https://images.pexels.com/photos/4725133/pexels-photo-4725133.jpeg'},
+        {id: 15, title: 'title', comments: '', src: 'https://dummyimage.com/600x400/000/000fe3'},
+        {id: 16, title: 'title', comments: '', src: 'https://dummyimage.com/600x400/000/ed0c0c'}
     ]
     // 문의사항은 https://jsonplaceholder.typicode.com/posts 여기다가 get 요청보내서 json받아온 걸로 더미데이터 사용해서
     // 사용할 것
--- client/src/web/components/contentsComponents/Gallery.jsx
+++ client/src/web/components/contentsComponents/Gallery.jsx
@@ -3,88 +3,81 @@ import styled from 'styled-components';
 import dummyPictures from '../../../dummyDate';
 
 function Gallery() {
-    
+
     const [forRenderingOne, setForRenderingOne] = useState(<div></div>);
     const [forRenderingTwo, setForRenderingTwo] = useState(<div></div>);
     const [forRenderingThree, setForRenderingThree] = useState(<div></div>);
     const [forRenderingFour, setForRenderingFour] = useState(<div></div>);
-
+    const [pageCount, setPageCount] = useState(<div>1</div>);
 
     // 애초에 4개씩 받아서 뿌릴 것
 
     function forFourMultiplyFour(_pictures) {
-        if(_pictures.length === 0){
+
+        if (_pictures.length === 0) {
             return '';
         }
-        for (let i = 0; i < 3; i++) {
-            _pictures.push(dummyPictures.pictures[i]);
-        }
+
         return <div
-            style={{ display: 'flex' }}>{_pictures.map((el) => {
-                return <div style={{ margin: '5px'}}key={el.id}><Img src={el.src} alt="picture"></Img></div>
+            style={{ display: 'flex', justifyContent: 'center' }}>{_pictures.map((el) => {
+                return <div style={{ margin: '5px' }}>
+                    <Img key={el.id} src={el.src} alt="picture"></Img>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <div>{el.title}</div>
+                        <div>생성일자</div>
+                    </div>
+                </div>;
             })}</div>;
+    }
+
+    function makeHowManyPage(count) {
+
+        let temp = ['안녕하세요', '안녕하세요', '안녕하세요', '안녕하세요'];
+
+        return <div>{
+        ()=>{
+            for(let i = 0; i < temp.length; i++){
+            <div>{temp[i]}</div>
+        }
+        }    
+        }</div>;
+
+        return <div 
+        className="makeHowManyPage"
+        style={{ display: 'flex', justifyContent: 'center' }}>
+            {() => {
+                for (let i = 1; i <= count; i++) {
+                    return <div>{i}</div>
+                }
+            }}
+        </div>;;
     }
 
     useEffect(() => {
         // 서버에서 줄때 무조건 객체 16개가 든 배열이 응답해와야 정상작동되는 코드다..
+        setPageCount(makeHowManyPage(5))
 
-        console.log('dummyPictures.pictures.slice(0,4) : ', dummyPictures.pictures.slice(0,4));
-        console.log('dummyPictures.pictures.slice(4,8) : ', dummyPictures.pictures.slice(4,8));
-        console.log('dummyPictures.pictures.slice(8,12) : ', dummyPictures.pictures.slice(8,12));
-        console.log('dummyPictures.pictures.slice(12) : ', dummyPictures.pictures.slice(12));
+        setForRenderingOne(forFourMultiplyFour(dummyPictures.pictures.slice(0, 4)));
+        setForRenderingTwo(forFourMultiplyFour(dummyPictures.pictures.slice(4, 8)));
+        setForRenderingThree(forFourMultiplyFour(dummyPictures.pictures.slice(8, 12)));
+        setForRenderingFour(forFourMultiplyFour(dummyPictures.pictures.slice(12)));
 
-        setForRenderingOne(forFourMultiplyFour(()=>{
-            if(dummyPictures.pictures.slice(0,4).length === 0){
-                return [];
-            }else{
-                return dummyPictures.pictures.slice(0,4);
-            } 
-        }));
-        setForRenderingTwo(forFourMultiplyFour(()=>{
-            if(dummyPictures.pictures.slice(4,8).length === 0){
-                return [];
-            }else{
-                return dummyPictures.pictures.slice(4,8);
-            }   
-        }));
-        setForRenderingThree(forFourMultiplyFour(()=>{
-            if(dummyPictures.pictures.slice(8,12).length === 0){
-                return [];
-            }else{
-                return dummyPictures.pictures.slice(8,12);
-            }
-        }));
-        setForRenderingFour(forFourMultiplyFour(()=>{
-            //dummyPictures.pictures.slice(12)
-            if(dummyPictures.pictures.slice(12).length === 0){
-                return [];
-            }else{
-                return dummyPictures.pictures.slice(12);
-            }
-        }));
-    }, [])
+    }, []);
+
     return (
-        <div style={{ textAlign: 'center'}}>
-                {forRenderingOne}
+            <div>
+                {/* {forRenderingOne}
                 {forRenderingTwo}
                 {forRenderingThree}
-                {forRenderingFour}
-        </div>
+                {forRenderingFour} */}
+                {pageCount}
+            </div>
     )
 }
 
 export default Gallery
 
 const Img = styled.img`
-    width: 20vw;
+    width: 15vw;
     height: 20vh;
 `
-
-/*
-                {dummyPictures.pictures.map((el)=> {
-                return <p>
-                    <Img key={el.id} src={el.src} alt="test"></Img>
-                    <div>{el.title}</div>
-                </p>
-            })}
-*/
