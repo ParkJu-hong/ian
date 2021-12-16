683929781a9bacc973e0363ab30a6d39f6c1fa02
bejeju <bejejupark@gmail.com>
2021-12-16 22:23:00 +0900

    21.12.16

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+
+// 서버 처음부터 구축하는 블로깅 할 것
