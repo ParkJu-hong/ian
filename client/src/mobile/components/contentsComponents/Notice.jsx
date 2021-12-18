22d1767b1ff63f21fed6d6ad8a81e827e3f31f5a
bejeju <bejejupark@gmail.com>
2021-12-18 23:50:17 +0900

    21.12.18

--- client/src/mobile/components/contentsComponents/Notice.jsx
+++ client/src/mobile/components/contentsComponents/Notice.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function Notice() {
     return (
         <div>
-            Notice
+            <h3 className="area_tit" style={{ marginLeft: '10vw'}}>이안환경 서비스 <br></br>공지사항</h3>
+            <hr></hr>
         </div>
     )
 }
