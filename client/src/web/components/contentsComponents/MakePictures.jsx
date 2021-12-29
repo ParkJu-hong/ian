81ede98a6d88098e5c13fef443d0daf49b29e7e9
bejeju <bejejupark@gmail.com>
2021-12-29 21:48:01 +0900

    21.12.29

--- client/src/web/components/contentsComponents/MakePictures.jsx
+++ client/src/web/components/contentsComponents/MakePictures.jsx
@@ -1,64 +1,56 @@
 import React, { useState, useEffect } from 'react';
-const AWS = require('aws-sdk');
-AWS.config.region = 'ap-northeast-2';
-// const formidable = require('formidable');
+import axios from 'axios';
 
-
-// s3에 이미지파일, 텍스트(문자열)올리고 볼 수 있는 Create, Read 까지 구현해보자.
-/*
-    npm install aws-sdk --save
-
-    그 후에 notion에 정리한 대로 개발환경을 구축했다.
-
-    npm install formidable@latest --save
-*/
 
 function MakePictures() {
+    /*
+        서버에서 S3로 이미지파일을 업로드하는 웹페이지를 html코드로 보내줄 것이다
+        이 컴포넌트에서는 그걸 ajax로 rest api get요청 사용해서 받고 뿌려주면 됨 
+    */
 
-    const [s3, setS3] = useState(null);
-    // const [clickOrNot, setClickOrNot] = useState(false);
-    const [forCleanUp, setForCleanUp] = useState(true);
+    // const [forRendering, setForRendering] = useState(<></>);
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    // onChange역할
+    const handleFileChange = (event) => {
+        console.log('handleFileChange => event.target : ', event.target.files[0]);
+        setSelectedFile(event.target.files[0]);
+    };
+    const handleFileUpload = () => {
+        const formData = new FormData();
+     
+        formData.append("userfile", selectedFile, selectedFile.name);
+
+        console.log('formData : ', Object.keys(formData).length === 0);
+
+        axios.post("http://localhost:3001/uploadfile", { formData }) // 여기서 {} 객체로 감싸면 req.body로 받을 수 있다.
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+        return;
+    }
 
     useEffect(() => {
-        if (forCleanUp) {
-            setS3(new AWS.S3());
-        }
-        return () => {
-            setForCleanUp(false);
-        }
+        // axios.get('http://localhost:3001/awssdk')
+        // .then((response)=>{
+        //     // console.log('response : ', response);
+        //     setForRendering(response.data);
+        // })
+        // .catch((error) => {
+        //     console.log(error);
+        //     setForRendering(<h1>error</h1>);
+        // })
     }, [])
 
     return (
         <div>
-            <form enctype="multipart/form-data" method="post">
-                <input type="file" name="userfile"></input>
-                <input type="submit"></input>
-            </form>
-            {/* <form enctype="multipart/form-data" method="post" name="fileinfo">
-                <label>Your email address:</label>
-                <input type="email" autocomplete="on" autofocus name="userid" placeholder="email" required size="32" maxlength="64" /><br />
-                <label>Custom file label:</label>
-                <input type="text" name="filelabel" size="12" maxlength="32" /><br />
-                <label>File to stash:</label>
-                <input type="file" name="file" required />
-                <input type="submit" value="Stash the file!" />
-            </form> */}
-            <h1 onClick={() => {
-                console.log(s3);
-                const params = {
-                    Bucket: 'ianenvironment',
-                    Key: 'ThisIsTest',
-                    ACL: 'public-read',
-                    Body: 'test.jpeg'
-                }
-                s3.upload(params, function (err, data) {
-                    if(err){
-                        console.log(err);
-                    }else{
-                        console.log(data);
-                    }
-                });
-            }}>Click Me !</h1>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleFileUpload}>업로드!</button>
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -5,7 +5,20 @@ const http = require('http');
 const cors = require('cors');
 // const cookieParser = require('cookie-parser');
 const express = require('express');
-const controllers = require('./controllers')
+const controllers = require('./controllers');
+require("dotenv").config();
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const SESConfig = {
+    apiVersion: "2015-03-31",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    accessSecretKey: process.env.AWS_SECRET_KEY,
+    region: "ap-northeast-2"
+}
+AWS.config.update(SESConfig);
+
+const S3 = new AWS.S3();
+
 
 const app = express();
 
@@ -19,6 +32,28 @@ app.use(cors({
 
 app.get('/gallery/read', controllers.read);
 app.post('/gallery/create', controllers.create);
+app.post('/uploadfile', (req, res) => {
+    S3.listBuckets(function (err, data) {
+        if (err) {
+            console.log("listBuckets Error", err);
+        } else {
+            console.log("listBuckets Success", data.Buckets);
+        }
+    });
+    console.log('req.body.formData : ', Object.keys(req.body.formData).length); 
+
+    // fs.readFile(req.files.file.path, function(err, data){
+    //     // Do something with the data (which holds the file information)
+    //     console.log('req.files.file.path : ', req.files.file.path);
+    //     if(err){
+    //         console.log('err : ', err);
+    //     }else{
+    //         console.log('data : ', data);
+    //     }
+    // });
+
+    res.status(201).send('OK')
+})
 
 app.listen(3001, () => {
     console.log('서버가 실행되었습니다')
